eb228647b50206d1357fd9258b9be6ec2d2401f5
Anne Kotecha <anne@olice.co.uk>
2018-04-19 13:47:22 +0100

    Began work on making the logos per row dynamic.

--- js/index.js
+++ js/index.js
@@ -1,81 +1,48 @@
-// TO DO NEXT:
-// On maximise, all items to go to their designated grid position
-
-
-// Create custom 'in' and 'out' animations
+// Custom 'in' and 'out' animations
 CustomEase.create("customIn", "M0,0 C0,0 0.136,0.462 0.172,0.902 0.182,0.99 0.234,1.048 0.312,1.044 0.363,1.041 0.394,0.978 0.468,0.978 0.524,0.978 0.58,1.01 0.638,1.012 0.726,1.012 0.766,1 0.904,1 0.964,1 1,1 1,1");
 CustomEase.create("customOut", "M0,0 C0.344,-0.06 0.544,0.091 0.686,0.198 0.888,0.35 0.99,0.638 1,1");
 
-// Get refs to elements
-var btn = document.querySelector('#btn');
-var btnMaximise = document.querySelector('#btn-maximise');
-var btnMinimise = document.querySelector('#btn-minimise');
-
-var logo_01 = document.querySelector('#logo-01');
-var logo_02 = document.querySelector('#logo-02');
-var logo_03 = document.querySelector('#logo-03');
-var logo_04 = document.querySelector('#logo-04');
-var logo_05 = document.querySelector('#logo-05');
-var logo_06 = document.querySelector('#logo-06');
-var logo_07 = document.querySelector('#logo-07');
-var logo_08 = document.querySelector('#logo-08');
-var logo_09 = document.querySelector('#logo-09');
-
-var allItems = [
-    logo_01, logo_02, logo_03,
-    logo_04, logo_05, logo_06,
-    logo_07, logo_08, logo_09
-];
-
-// The main timeline
-var master;
+// DOM elements
+//---------------------------------//
+const allItems = document.getElementsByClassName('logo');
+let master;
 
 
-btn.addEventListener('click', function() {
-    createAnimation();
-});
-
-btnMaximise.addEventListener('click', function() {
-    maximise();
-});
-
-btnMinimise.addEventListener('click', function() {
-    minimise();
-});
-
-// ------------------ //
 // Animation variables
-// ------------------ //
-var timing = {
+//---------------------------------//
+const timing = {
     in:1,
     out:1,
     betweenRows: '-=2',
     betweenLoop: 2
 }
 
-var startY = 150;
-var endY = 50;
-var logosPerRow = 3;
-var spacingX = 220;
-var spacingY = 130;
-// ------------------ //
-// ------------------ //
+const startY = 150;
+const endY = 50;
+const logosPerRow = 2;
+const spacingX = 220;
+const spacingY = 130;
 
 
+// Main methods
+//---------------------------------//
 function createAnimation(){
 	initAnim();
-	// Create a master timeline
-	// With a timeline for each row
+	// Create a master timeline with a timeline for each row
 	master = new TimelineMax({paused:true});
 	master.add(rowAnim(1));
-    master.add(rowAnim(2), timing.betweenRows);
-    master.add(rowAnim(3), timing.betweenRows);
+    const rowCount = getRowCount();
+    console.log('rowCount ', rowCount);
+    for (let i=2; i<rowCount; i++){
+         master.add(rowAnim(i), timing.betweenRows);
+    }
+    // master.add(rowAnim(2), timing.betweenRows);
+    // master.add(rowAnim(3), timing.betweenRows);
 	master.play();
 
 }
 
 function initAnim(){
-
 	// Set logos at their correct x positions
     var xMarker = 0;	
     for (var i=0; i<allItems.length; i++){
@@ -92,117 +59,117 @@ function initAnim(){
     }
 }
 
-// Returns a timeline for one row
+
 function rowAnim(num) {
-
+    // Returns a timeline for one row
     var tl = new TimelineMax({repeat:-1,repeatDelay:timing.betweenLoop*3});
-    // Wait 12 seconds before playing again
-    // (each anim takes 6 secs and there are 2 row animations to wait for)
-    // var tl = new TimelineMax({repeat:-1,repeatDelay:12});
 
-    var marker;
-    if (num === 1) {marker=0;}
-    if (num === 2) {marker=3;}
-    if (num === 3) {marker=6;}
+    // var marker;
+    // if (num === 1) {marker=0;}
+    // if (num === 2) {marker=3;} // If we're on row 2, get items starting at index 3
+    // if (num === 3) {marker=6;}
+    let marker = getRowStartIndex(num,logosPerRow);
 
     // Select next set of logos to animate
-    var elems = [allItems[marker], allItems[marker + 1], allItems[marker + 2]];
+    // var elems = [allItems[marker], allItems[marker + 1], allItems[marker + 2]];
+    let elems = [];
+    for (let k=0; k<logosPerRow; k++){
+        elems.push(allItems[marker+k]);
+    }
 
-    // Set their position to start y position
-    // tl.set(element, {left:100, opacity:0.5});
-
-    // Tween in Row 1
     tl.add('animate-in');
-    tl.fromTo(elems[0], timing.in ,{y:startY, opacity:0},{ease:"customIn",y:endY, opacity:1});
-    tl.fromTo(elems[1], timing.in, {y:startY, opacity:0},{ease:"customIn",y:endY, opacity:1});
-    tl.fromTo(elems[2], timing.in, {y:startY, opacity:0},{ease:"customIn",y:endY, opacity:1});
+    for (let i=0; i<logosPerRow; i++) {
+        console.log('elem ', elems[i]);
+        tl.fromTo(elems[i], timing.in ,{y:startY, opacity:0},{ease:"customIn",y:endY, opacity:1});
+    }
    
-    // // Tween out Row 1
     tl.add('animate-out');
-    tl.to(elems[0], timing.out, {ease:"customOut",y:startY, opacity:0});
-    tl.to(elems[1], timing.out, {ease:"customOut",y:startY, opacity:0});
-    tl.to(elems[2], timing.out, {ease:"customOut",y:startY, opacity:0});
+    for (let j=0; j<logosPerRow; j++) {
+        console.log('elem ', elems[j]);
+       tl.to(elems[j], timing.out, {ease:"customOut",y:startY, opacity:0});
+    }
 
     return tl;
 }
 
 
-
-
-// Pause and close down existing animation
-// Shuffle up remaining rows
-// Expand 'logo container' div
-function maximise(){
-	 master.pause();
-     itemsToGrid();
+function getRowCount() {
+    return Math.floor(allItems.length/logosPerRow);
 }
 
-function minimise(){
-    itemsToStart();
-    // master.play();
+function getRowStartIndex(num, rowMax) {
+    return Math.floor((num-1)*rowMax);
+    // console.log('start ', start + ' ' + num + ' ' + rowMax)
 }
 
-// Each logo has an assigned x,y position with the 'grid'
-// xpos always stays the same, assign a new y pos here
-// Move them from where they are to the destination position.
-function itemsToGrid() {
-    var elems = allItems;
-    var startPos = 100;
-    var markerX = 0;
-    var markerY = 0;
-    var posY = 0;
-    for (var i = 0; i < elems.length; i++) {
 
-        // Establish a y pos for this item
-        posY = startPos + (markerY * spacingY)
+// Maximise / minimise functionality
+//---------------------------------//
+// var btn = document.querySelector('#btn');
+// var btnMaximise = document.querySelector('#btn-maximise');
+// var btnMinimise = document.querySelector('#btn-minimise');
 
-        // When we have reached max num of logos per row
-        // Set x marker back to 0;
-        // Increment y marker
-        if (markerX === (logosPerRow - 1)) {
-            markerX = 0;
-            markerY++;
-        } else {
-            // Otherwise, just increment x marker
-            markerX++;
-        }
-        // Set logo at xpos
-        TweenMax.to(elems[i], 0.6, { opacity:1, y: posY, ease: Power1.easeIn });
-    }
-}
+// btnMaximise.addEventListener('click', function() {
+//     maximise();
+// });
 
-// Move logos back to their starting y positions
-function itemsToStart(){
-    // for (var i=0; i<allItems.length; i++){
-        TweenMax.staggerTo(allItems, 0.6, {opacity:0, y: startY, ease: Power1.easeIn, onComplete:restart});
-    // }
-}
+// btnMinimise.addEventListener('click', function() {
+//     minimise();
+// });
 
-function restart() {
-    master.play();
-}
 
-function itemsToFixedGrid () {
-
-}
-
-// // Utils / helper functions
-// function randomise(array) {
-//     var currentIndex = array.length,
-//         temporaryValue, randomIndex;
-
-//     // While there remain elements to shuffle...
-//     while (0 !== currentIndex) {
-
-//         // Pick a remaining element...
-//         randomIndex = Math.floor(Math.random() * currentIndex);
-//         currentIndex -= 1;
-
-//         // And swap it with the current element.
-//         temporaryValue = array[currentIndex];
-//         array[currentIndex] = array[randomIndex];
-//         array[randomIndex] = temporaryValue;
-//     }
-
-//     return array;
+// // Pause and close down existing animation
+// // Shuffle up remaining rows
+// // Expand 'logo container' div
+// function maximise(){
+// 	 master.pause();
+//      itemsToGrid();
 // }
+
+// function minimise(){
+//     itemsToStart();
+// }
+
+// // Each logo has an assigned x,y position with the 'grid'
+// // xpos always stays the same, assign a new y pos here
+// // Move them from where they are to the destination position.
+// function itemsToGrid() {
+//     var elems = allItems;
+//     var startPos = 100;
+//     var markerX = 0;
+//     var markerY = 0;
+//     var posY = 0;
+//     for (var i = 0; i < elems.length; i++) {
+
+//         // Establish a y pos for this item
+//         posY = startPos + (markerY * spacingY)
+
+//         // When we have reached max num of logos per row
+//         // Set x marker back to 0; increment y marker
+//         if (markerX === (logosPerRow - 1)) {
+//             markerX = 0;
+//             markerY++;
+//         } else {
+//             // Otherwise, just increment x marker
+//             markerX++;
+//         }
+//         // Set logo at xpos
+//         TweenMax.to(elems[i], 0.6, { opacity:1, y: posY, ease: Power1.easeIn });
+//     }
+// }
+
+// // Move logos back to their starting y positions
+// function itemsToStart(){
+//         TweenMax.staggerTo(allItems, 0.6, {opacity:0, y: startY, ease: Power1.easeIn, onComplete:restart});
+// }
+
+// function restart() {
+//     master.play();
+// }
+
+
+// Init
+//---------------------------------//
+window.onload = function() {
+  createAnimation();
+};
